bc5c3bb38db766f04482cbfadb29818075229b0b
ppuacgarcia <55265850+ppuacgarcia@users.noreply.github.com>
2022-10-25 16:47:58 -0600

    develop:update
    some changes

--- script.js
+++ script.js
@@ -2,42 +2,42 @@
 //Pablo David Puac Garcia
 //script for connect a pokemon API 
 for(var i=0;i<6;i++){
-const request = new XMLHttpRequest();
-var pokernd=Math.floor(Math.random()*890)
-console.log(pokernd)
-request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+pokernd+'/', true);
-
-request.onload = function () {
+  const request = new XMLHttpRequest();
+  var pokernd=Math.floor(Math.random()*890)
+  console.log(pokernd)
+  request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+pokernd+'/', true);
+  request.onload = function () {
   if (request.status >= 200 && request.status < 400) {
     const data = JSON.parse(this.response);
     //tarjeta
-    const tarjeta = document.createElement('div');
-    const contenedor = document.getElementById('contenedor');
-    tarjeta.setAttribute('class', 'card');
-    // Creamos el la cabecera y el cuerpo de la tarjeta
-    const cabeceraTarjeta = document.createElement('div');
-    cabeceraTarjeta.setAttribute('class', 'card-header');
-    const cuerpoTarjeta = document.createElement('div');
-    cuerpoTarjeta.setAttribute('class', 'card-body')
-    //nombre pokemon
-    const titulo = document.createElement('h5');
-    titulo.setAttribute('class', 'card-title');
-    titulo.textContent = data.species.name;
+      const card = document.createElement('div');
+      const container = document.getElementById('containerc');
+      card.setAttribute('class', 'card');
+    // Create card body and heade,
+      const cardHeader = document.createElement('div');
+      cardHeader.setAttribute('class', 'card-header');
+      const cardBody = document.createElement('div');
+      cardBody.setAttribute('class', 'card-body')
+    //pokemon set name
+      const pokeName = document.createElement('h5');
+      pokeName.setAttribute('class', 'card-title');
+      pokeName.textContent = data.species.name;
     //pokemon sprite
-    const descripcion = document.createElement('img');
-    descripcion.setAttribute('class', 'card-text');
-    descripcion.setAttribute('src',data.sprites.front_default);
-    contenedor.appendChild(tarjeta);
-    tarjeta.appendChild(cabeceraTarjeta);
-    tarjeta.appendChild(cuerpoTarjeta);
-    // Agregamos el título a la cabecera
-    cabeceraTarjeta.appendChild(titulo);
-     // Agregamos la descripción al cuerpo
-     cuerpoTarjeta.appendChild(descripcion);
-  } else {
+      const sprite = document.createElement('img');
+      sprite.setAttribute('class', 'card-text');
+      sprite.setAttribute('src',data.sprites.front_default);
+    //insert elements
+      container.appendChild(card);
+      card.appendChild(cardHeader);
+      card.appendChild(cardBody);
+    // add the pokemon name
+      cardHeader.appendChild(pokeName);
+    // Add the pokemon spritec
+      cardBody.appendChild(sprite);
+  }else{
     console.log('Ha ocurrido un error con código ' + request.status);
   }
-}
+  }
 
-  request.send();
+    request.send();
 }
